eeacb17f84a14f6eb4b8ecefecd1f09f8b59a299
julie-anz <47120217+julie-anz@users.noreply.github.com>
2025-12-06 20:49:41 -0500

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -4,7 +4,7 @@
 -- DML file for CRUD operations
 
 
-
+---- READ Queries ----
 
 -- Select all users
 SELECT
@@ -16,8 +16,6 @@ SELECT
 FROM Users;
 
 
-
-
 -- Select all resources
 SELECT
   r.resourceID,
@@ -27,55 +25,6 @@ SELECT
   u.lastName as 'Owner Last Name'
 FROM Resources r
 JOIN Users u on r.userID = u.userID ;
-
-
-
-
--- -- Search for a resource
--- SELECT
---    r.resourceID,
---    r.resourceName as Name,
---    r.resourceDescription as Description,
---    u.firstName as 'Owner First Name',
---    u.lastName as 'Owner Last Name'
--- FROM Resources r
--- JOIN Users u on r.userID = u.userID
---    WHERE r.resourceName = @resourceName
---    OR r.resourceDescription ILIKE %@resourceDescription%;
-
-
-
-
--- Add a resource
-INSERT INTO Resources (userID, resourceName, resourceDescription)
-VALUES(
-  userID = (SELECT userID FROM Users WHERE userID = @userID),
-  resourceName = @resourceName,
-  resourceDescription = @resourceDescription
-);
-
-
-
-
--- -- Select current active loans by user
--- SELECT
---    l.loanID,
---    l.startDate as 'Start Date',
---    l.dueDate as 'Due Date',
---    r.resourceName as 'Resource Name',
---    u.firstName as 'Lender First Name',
---    u.lasttName as 'Lender Last Name'
--- FROM Loans l
--- JOIN Users u on l.userID = u.userID
--- JOIN Resources r on l.resourceID = r.resourceID
---    WHERE l.returnedDate = NULL
---    AND l.userID =
---        (SELECT userID
---        FROM Users
---        WHERE email = @email
---        );
-
-
 
 
 -- Select all loans
@@ -91,19 +40,6 @@ JOIN Users u on l.userID = u.userID
 JOIN Resources r on l.resourceID = r.resourceID;
 
 
-
-
--- -- Search Locations
--- SELECT
---    locationID,
---    locationName,
---    locationDescription
--- FROM Locations
---    WHERE locationDescription ILIKE %@locationDescription%;
-
-
-
-
 -- Select all locations
 SELECT
   locationID,
@@ -112,9 +48,7 @@ SELECT
 FROM Locations;
 
 
-
-
--- Select all ResourceLocation
+-- Select all ResourceLocations
 SELECT
   rl.ResourceLocationsID,
   r.resourceName,
@@ -124,9 +58,28 @@ JOIN Resources r on rl.resourceID = r.resourceID
 JOIN Locations l on rl.locationID = l.locationID;
 
 
+---- CREATE Queries ----
+
+-- Add a user
+INSERT INTO Users (firstName, lastName, email, phone)
+   VALUES (@firstName, @lastName, @email, @phone);
 
 
--- Insert a ResourceLocation
+-- Add a resource
+INSERT INTO Resources (userID, resourceName, resourceDescription)
+VALUES(
+  userID = (SELECT userID FROM Users WHERE userID = @userID),
+  resourceName = @resourceName,
+  resourceDescription = @resourceDescription
+);
+
+
+-- Add a location
+INSERT INTO Locations (locationName, locationDescription)
+   VALUES(@locationName, @locationDescription);
+
+
+-- Add a ResourceLocation (M:M)
 INSERT INTO ResourceLocations (resourceID, locationID)
 VALUES(
   (SELECT resourceID FROM Resources WHERE resourceID = @resourceID),
@@ -134,9 +87,56 @@ VALUES(
 );
 
 
+-- Add a loan
+INSERT INTO Loans (startDate, dueDate, returnedDate, userID, resourceID)
+   VALUES(
+     @startDate,
+     @dueDate,
+     @returnedDate,
+     (SELECT userID FROM Users WHERE userID = @userID),
+     (SELECT resourceID FROM Resources WHERE resourceID = @resourceID)
+   );
 
 
--- Update a ResourceLocation's locationID
+---- DELETE Queries ----
+
+-- Delete a ResourceLocation (M:M)
+DELETE FROM ResourceLocations
+WHERE
+ resourceID =  (SELECT resourceID FROM Resources WHERE resourceDescription = @resourceDescription)
+ AND locationID =  (SELECT locationID FROM Locations WHERE locationName = @locationName);
+
+
+-- Delete a Resource
+DELETE FROM Resources
+  WHERE resourceID = @resourceID
+
+
+-- Delete a User
+DELETE FROM Users
+  WHERE userID = @userID
+
+
+-- Delete a Loan
+DELETE FROM Loans
+  WHERE loanID = @loanID
+
+
+-- Delete a Loaction
+DELETE FROM Locations
+  WHERE LoactionID = @locationID
+
+
+
+---- UPDATE Queries ----
+  
+-- Update a Resource Description
+UPDATE Resources
+SET resourceDescription = @resourceDescription
+WHERE resourceID = @resourceID
+
+
+-- Update a ResourceLocation's locationID (M:M)
 UPDATE ResourceLocations
 SET
 locationID = (SELECT locationID FROM Locations WHERE locationID = @locationID)
@@ -146,48 +146,4 @@ WHERE
 
 
 
--- Delete a ResourceLocation
-DELETE FROM ResourceLocations
-WHERE
- resourceID =  (SELECT resourceID FROM Resources WHERE resourceDescription = @resourceDescription)
- AND locationID =  (SELECT locationID FROM Locations WHERE locationName = @locationName);
-
-
-
-
--- Delete a Resource
-DELETE FROM Resources
-  WHERE resourceID = @resourceID
-  
--- Update a Resource Description
-UPDATE Resources
-SET resourceDescription = @resourceDescription
-WHERE resourceID = @resourceID
-
-
--- -- Update a Resource Name
--- UPDATE Resources
--- SET resourceName = @resourceName
--- WHERE resourceID = @resourceID
-
-
--- add a user
-INSERT INTO Users (firstName, lastName, email, phone)
-   VALUES (@firstName, @lastName, @email, @phone);
-
-
--- add a location
-INSERT INTO Locations (locationName, locationDescription)
-   VALUES(@locationName, @locationDescription);
-
-
--- add a loan
-INSERT INTO Loans (startDate, dueDate, returnedDate, userID, resourceID)
-   VALUES(
-     @startDate,
-     @dueDate,
-     @returnedDate,
-     (SELECT userID FROM Users WHERE userID = @userID),
-     (SELECT resourceID FROM Resources WHERE resourceID = @resourceID)
-   );
 
